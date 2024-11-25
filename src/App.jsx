44ac6fe0a7ee3914174f732a726c3fb950eb8ba5
jerome-11-png <jerome@miiapps.com>
2024-11-25 14:40:31 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React, { useState } from 'react'
 
 const NAVIGATION_CARDS = [
   { icon: "🔍", title: "DeepFake Detection", url: "https://noumanjavaid-centurion.hf.space" },
-  { icon: "📄", title: "Document Analysis", url: "https://noumanjavaid-centurionv2.hf.space" },
-  { icon: "🎥", title: "Image Watermarking", url: "https://renesistech-watermaking-centurion-final.hf.space" },
-  { icon: "🔐", title: "AI Detection ", url: "https://renesistech-aidetection.hf.space" },
-  { icon: "🖼️", title: "Image Comparison", url: "https://noumanjavaid-centii.hf.space" }
+  { icon: "📄", title: "Document Analysis", url: "https://renesistech-doc-analysis.hf.space" },
+  { icon: "🔐", title: "Image Authentication", url: "https://renesistech-demo-ate.hf.space" },
+  { icon: "🖼️", title: "Image Watermarking", url: "https://renesistech-watermaking-centurion-final.hf.space" },
+  { icon: "⚠️", title: "AI Detection", url: "https://renesistech-aidetection.hf.space" }
 ]
  
 Image Analysis
